55af9fc737e7999b3104c2e12ebb0307cade8807
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-06-10 02:50:33 +0530

    chore: mount userinfo router in express app

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import path from "node:path";
 import { discoveryRouter } from "./modules/discovery/discovery.routes.js";
 import { authRouter } from "./modules/auth/auth.routes.js";
 import { tokenRouter } from "./modules/token/token.routes.js";
+import { userinfoRouter } from "./modules/userinfo/userinfo.routes.js";
 import { errorHandler } from "./common/middleware.js";
 
 const app = express();
@@ -23,6 +24,7 @@ app.get("/health", (_, res) =>
 app.use(discoveryRouter);
 app.use(authRouter);
 app.use(tokenRouter);
+app.use(userinfoRouter);
 
 app.use(errorHandler as express.ErrorRequestHandler);
 
